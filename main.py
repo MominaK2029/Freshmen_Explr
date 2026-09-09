d8d4adb7ca598f1368d50a41f398763447c5fe14
MominaK2029 <student.165557@worcesterschools.net>
2026-09-09 18:31:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+#Prologue
 print("In this little town....")
-
+print(" ")
 print("Everything was perfect.")
-
+print(" ")
 print("Until, an invasive species from the unknown spread into our land.")
-
+print(" ")
 print("Those who preyed upon the species, were infected. They couldn't find the source. Yet those beings seemed to shift. Minor mutations over time, until they were unrecognizable. The chain eventually reached humans. The humans who consumed the infection walked unaware. Soon the disease became air-born.")
-
+print(" ")
 print("And that's when it happened. The lockdown. The Apocalypse.")
-
+print(" ")
 print("Humans began turning to their primitive ways. We were spread across the land. My parents were severely ill. They haven't found a cure for the disease. My sister and I have no idea what to do. We quarantined them in the basement. There have been some speculation on a cure but until then, we don't know what to do.")
-
+print(" ")
 print("I plan to go out tomorrow. I have to find more rations for the month.")
-
+print(" ")
 print("I head to bed. Tomorrow will hopefully be good...")
-
+print(" ")
 # Game Begins
 print("*")
 print("**")
 print("***")
+print(" ")
+# Character name
+name = input("Enter your name...")
+
+print(name, "..")
+print(name, "...")
+print(name.upper())
 
 # Character name
 name = input("Enter your name...")
@@ -25,4 +33,46 @@ name = input("Enter your name...")
 print(name, "..")
 print(name, "...")
 print(name.upper())
+
+#First pinpoint
+choice1 = input("Wake up (A) or Sleep in (B)")
+
+#SL 1, 3, 4
+if choice1 == "A":
+  print(name, "woke up to their sister.")
+  print(" ")
+  print("'C'mon you have to be early to avoid the zombies! Mom and Dad start their rounds and attempts at around 5pm and it's 1 right now. Get up.'")
+  print(" ")
+  print(name, "got up and made sure they had the proper gear on.", name, "looked at their pistol")
+  #Second pinpoint
+  choice2 = input("Leave the pistol and carry a knife instead (A) or Carry pistol and Knife (B)")
+  #SL 1, 4
+  if choice2 == "A":
+    print("You went out in search of supplies and carried on through the woods gathering resources that managed to last.", name "raided a grocery store last time but they seemed to find more resources in the other companies...")
+    print(" ")
+    print("'Sis, I'm home.'", name "shouts out. Silence, weird.")
+    print("You go to grab your gun but it has dissappeared")
+    choice3 = input("What do you do? Explore the area (A) or get out of the house with your supplies (B) .")
+    #SL 1
+    if choice3 ==  "A":
+      print(name, "ran out of the house with their supplies and decided to run away. The terror may have ended. But hardships are still in your heart. Ending…")
+      print(“1”)
+    #SL 1, 4
+    elif choice3 == "B":
+        print("You choose to explore the area. Your sister must be playing.")
+        print("You find your sister, she's standing eerily.")
+        print("'Sister'", name, "calls out. She starts moving forward. You have to make a decision")
+        choice4 = input("Do you fight your way out (A) or just give up and get infected (B) ?")
+        #SL1 final choice
+        if choice4 == "A":
+          print("You ran away while you still had the chance. Your family is now gone. You're on your own. Ending…")
+        print(“1”)
+      #SL 3 + 4
+  if choice2 == "B":
+    print("You took both with you. Just in case.")
+    print("The outside was dangerous. You never know what you might come across.")
+    print(" ")
+    print("You stumbled upon an unknown base, the facial recognition scans your face. 
+    
+        
 
