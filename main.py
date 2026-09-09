320bb205553f72ca726ff0eb5d63a5153f298e52
MominaK2029 <student.165557@worcesterschools.net>
2026-09-08 22:08:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
-print("In this little town....")
+print("In this little town...."/n)
 
-print()
+print("Everything was perfect."/n)
 
-print("Everything was perfect.")
+print("Until, an invasive species from the unknown spread into our land."/n)
 
-print()
+print("Those who preyed upon the species, were infected. They couldn't find the source. Yet those beings seemed to shift. Minor mutations over time, until they were unrecognizable. The chain eventually reached humans. The humans who consumed the infection, walked unaware. Soon the disease became air-born."/n)
 
-print("Until, an invasive species from the unknown spread into our land.")
+print("And that's when it happened. The lockdown. The Apocalypse."/n)
 
-print()
+print("Humans began turning to their primative ways. We were spread across the land. My parents were severly ill. They haven't found a cure for the disease. My sister and I have no idea what to do. We quarentined them to the basement. There have been some speculation on a cure but until then, we don't know what to do."/n)
 
-print("Those who preyed upon the species, were infected. They couldn't find the source. Yet those beings seemed to shift. Minor mutations over time, until they were unrecognizable. The chain eventually reached humans. The humans who consumed the infection, walked unaware. Soon the disease became air-born.")
+print("I plan to go out tommorrow. I have to find more rations for the month."/n)
 
-print()
-
-print("And that's when it happened. The lockdown. The Apocalypse.")
-
-print("Humans began turning to their primative ways. We were spread across the land. My parents were severly ill. They haven't found a cure for the disease. My sister and I have no idea what to do. We quarentined them to the basement. There have been some speculation on a cure but until then, we don't know what to do.")
-
-print()
-
-print("I plan to go out tommorrow. I have to find more rations for the month.")
-
-print()
-
-print("I head to bed. Tomorrow will hopefully be good...")
+print("I head to bed. Tomorrow will hopefully be good..."/n)
 
 # Game Begins
 print("*")
@@ -34,8 +22,8 @@ print("***")
 # Character name
 name = input("Enter your name...")
 
-print(name, "..")
-print(name, "...")
+print(name, ".."/n)
+print(name, "..."/n)
 print(name.upper())
 
 #First pinpoint
