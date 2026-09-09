57fe5c65caa05c6981442911be88db3438ef65f8
MominaK2029 <student.165557@worcesterschools.net>
2026-09-09 18:54:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -48,31 +48,48 @@ if choice1 == "A":
   choice2 = input("Leave the pistol and carry a knife instead (A) or Carry pistol and Knife (B)")
   #SL 1, 4
   if choice2 == "A":
-    print("You went out in search of supplies and carried on through the woods gathering resources that managed to last.", name "raided a grocery store last time but they seemed to find more resources in the other companies...")
+    print(name, "went out in search of supplies and carried on through the woods gathering resources that managed to last.", name, "raided a grocery store last time but they seemed to find more resources in the other companies...")
     print(" ")
-    print("'Sis, I'm home.'", name "shouts out. Silence, weird.")
-    print("You go to grab your gun but it has dissappeared")
+    print("'Sis, I'm home.'", name, "shouts out. Silence, weird.")
+    print("They go to grab their gun but it has dissappeared")
     choice3 = input("What do you do? Explore the area (A) or get out of the house with your supplies (B) .")
     #SL 1
     if choice3 ==  "A":
-      print(name, "ran out of the house with their supplies and decided to run away. The terror may have ended. But hardships are still in your heart. Ending…")
+      print(name, "ran out of the house with their supplies and decided to run away. The terror may have ended. But hardships are still in their heart. Ending…")
       print(“1”)
     #SL 1, 4
     elif choice3 == "B":
-        print("You choose to explore the area. Your sister must be playing.")
-        print("You find your sister, she's standing eerily.")
+        print("You choose to explore the area.", name, "sister must be playing.")
+        print("They find their sister, she's standing eerily.")
         print("'Sister'", name, "calls out. She starts moving forward. You have to make a decision")
         choice4 = input("Do you fight your way out (A) or just give up and get infected (B) ?")
         #SL1 final choice
         if choice4 == "A":
           print("You ran away while you still had the chance. Your family is now gone. You're on your own. Ending…")
         print(“1”)
+    else:
+      choice4 == "B"
+      print("You came this far. It was too difficult. Maybe it was time to..")
+      print("The world fades to black. Ending...")
+      print("4")
       #SL 3 + 4
   if choice2 == "B":
     print("You took both with you. Just in case.")
     print("The outside was dangerous. You never know what you might come across.")
     print(" ")
-    print("You stumbled upon an unknown base, the facial recognition scans your face. 
+    print("You stumbled upon an unknown base, the facial recognition scans your face.")
+    print(" ")
+    print("The base was wonderful. You met so many survivors on your trip. And the cure.")
+    print(" ")    
+    print("But it only worked on newly infected or safe people. Not those who have been morphed.")    
+    print(" ")    
+    print("You made your way back home. Your sister stood in the kitchen. A bit purple...")
+    print(" ")
+    print("'Sis?'", name," called out. You sister turned around, she was turning. You looked at the cure")
+    choice5 = input("Do you negotiate (A) or inject her with the cure (B)?")
+    #SL3 Final decision
+    if choice 5 == "B":
+      print("You moved quick. Before she could even react, you in
     
         
 
