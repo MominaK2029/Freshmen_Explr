77dc5e1a25c7bbede5cc918bbd1d0d19b157b946
MominaK2029 <student.165557@worcesterschools.net>
2026-09-09 15:35:49 -0400

    Base done

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-print("In this little town...."/n)
+print("In this little town....")
 
-print("Everything was perfect."/n)
+print("Everything was perfect.")
 
-print("Until, an invasive species from the unknown spread into our land."Ln)
+print("Until, an invasive species from the unknown spread into our land.")
 
-print("Those who preyed upon the species, were infected. They couldn't find the source. Yet those beings seemed to shift. Minor mutations over time, until they were unrecognizable. The chain eventually reached humans. The humans who consumed the infection, walked unaware. Soon the disease became air-born."/n)
+print("Those who preyed upon the species, were infected. They couldn't find the source. Yet those beings seemed to shift. Minor mutations over time, until they were unrecognizable. The chain eventually reached humans. The humans who consumed the infection walked unaware. Soon the disease became air-born.")
 
-print("And that's when it happened. The lockdown. The Apocalypse."/n)
+print("And that's when it happened. The lockdown. The Apocalypse.")
 
-print("Humans began turning to their primative ways. We were spread across the land. My parents were severly ill. They haven't found a cure for the disease. My sister and I have no idea what to do. We quarentined them to the basement. There have been some speculation on a cure but until then, we don't know what to do."/n)
+print("Humans began turning to their primitive ways. We were spread across the land. My parents were severely ill. They haven't found a cure for the disease. My sister and I have no idea what to do. We quarantined them in the basement. There have been some speculation on a cure but until then, we don't know what to do.")
 
-print("I plan to go out tommorrow. I have to find more rations for the month."/n)
+print("I plan to go out tomorrow. I have to find more rations for the month.")
 
-print("I head to bed. Tomorrow will hopefully be good..."/n)
+print("I head to bed. Tomorrow will hopefully be good...")
 
 # Game Begins
 print("*")
@@ -22,33 +22,7 @@ print("***")
 # Character name
 name = input("Enter your name...")
 
-print(name, ".."/n)
-print(name, "..."/n)
+print(name, "..")
+print(name, "...")
 print(name.upper())
 
-#First pinpoint
-choice1 = input("Wake up (A) or Sleep in (B)")
-
-if choice1 == "A":
-  print(name, "woke up to their sister.")
-  print(" ")
-  print("'C'mon you have to be early to avoid the zombies! Mom and Dad start their rounds and attempts at around 5pm and its 1 right now. Get up.'")
-  print(" ")
-  print(name, "got up and made sure they had the proper gear on.", name, "looked at their pistol")
-  #Second pinpoint
-  choice2 = input("Leave the pistol and carry a knife instead (A) or Carry pistol and Knife (B)")
-  if choice2 == "A":
-    print("You went out in search of supplies and carried on through the woods gathering resources that managed to last.", name "raided a grocery store last time but they seemed to find more resources in the other companies...")
-    print(" ")
-    print("'Sis, I'm home.'", name "shouts out. Silence, weird.")
-    print("You go to grab your gun but it has dissappeared")
-    choice3 = input("What do you do? Get out of the house (A) or Explore the area.")
-    if choice3 ==  "B":
-      print(name, "ran out of the house with their supplies and decided to run away. The terror may have ended. But hardships are still in your heart.")
-    elif choice3 == "A":
-        print("You choose to explore the area. Your sister must be playing.")
-        print("You find your sister, she's standing eerily.")
-        print("'Sister'", name, "calls out. She starts moving forward. You have to make a decision")
-        choice4 = input("Do you run (A) or just give up and get infected")
-        if choice4 == "A":
-          print("You ran away while you still had the chance. Your family is now gone. Your on your own.")
